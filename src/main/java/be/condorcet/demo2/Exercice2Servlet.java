da3fd160c88ec083148e884bbdace67fa931accd
Alan ngateu <m.ngateutagne@condorcet.be>
2026-09-21 13:13:29 +0200

    troisieme commit

--- src/main/java/be/condorcet/demo2/Exercice2Servlet.java
+++ src/main/java/be/condorcet/demo2/Exercice2Servlet.java
@@ -7,31 +7,44 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "Exercice2Servlet", value = "/Exercice2Servlet")
-public class Exercice2Servlet extends HttpServlet {
+@WebServlet(
+        name = "Exercice1Servlet",
+        value = "/Exercice1Servlet",
+        initParams = {
+                @WebInitParam(name="maintenaceMode", value = "true")
+        }
+)
+public class Exercice1Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String name = request.getParameter("nom");
-        String ageText = request.getParameter("age");
-        try{
-            int age =  Integer.parseInt(ageText);
-            PrintWriter out = response.getWriter();
-            out.println("<html>");
-            out.println("<head><title>Ma premier servlet</title></head>");
-            out.println("<body>");
-            if(age>18){
-                out.println("<h1>Bienvenue " + name +"</h1>");
-                out.println("<p>vous pouvez passer votre permis</p>");
+        String tes = getInitParameter("maintenaceMode");
+        boolean resultest = Boolean.parseBoolean(tes);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Ma premier servlet</title></head>");
+        out.println("<body>");
+        if(!resultest){
+            String name = request.getParameter("nom");
+            String ageText = request.getParameter("age");
+            try{
+                int age =  Integer.parseInt(ageText);
+                if(age>18){
+                    out.println("<h1>Bienvenue " + name +"</h1>");
+                    out.println("<p>vous pouvez passer votre permis</p>");
+                }
+                else{
+                    out.println("<p>j'espère que tu as skateboard!!!</p>");
+                }
+            }catch (NumberFormatException e){
+                out.println("<h1> erreur de format");
             }
-            else{
-                out.println("<p>j'espère que tu as skateboard!!!</p>");
-            }
-            out.println("</body>");
-            out.println("</html>");
-        }catch (NumberFormatException e){
-            PrintWriter out = response.getWriter();
-            out.println("<h1> erreur de format");
         }
+        else{
+            out.println("<p> Désolé nous somme en maintenance!!!</p>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+
 
     }
 

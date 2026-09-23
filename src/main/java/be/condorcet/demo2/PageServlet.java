8b2760a378ab9a5112008bfad3782e46d5ebca91
Alan ngateu <m.ngateutagne@condorcet.be>
2026-09-23 06:27:24 +0200

    exercice 3 commit

---
+++ src/main/java/be/condorcet/demo2/PageServlet.java
@@ -0,0 +1,18 @@
+package be.condorcet.demo2;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PageServlet", value = "/PageServlet")
+public class PageServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
